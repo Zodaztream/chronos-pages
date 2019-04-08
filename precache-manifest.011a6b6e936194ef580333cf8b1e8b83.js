70de7dc247d386d02120c0a7324e23151928c25c
Zodaztream <felno889@student.liu.se>
2019-04-08 12:19:46 +0200

    Updates

---
+++ precache-manifest.011a6b6e936194ef580333cf8b1e8b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chronos-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "21640014728a3ba9ffe6",
+    "url": "/chronos-pages/static/js/runtime~main.d492794d.js"
+  },
+  {
+    "revision": "2b3793b2b06719a024f8",
+    "url": "/chronos-pages/static/js/main.16916a9c.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/chronos-pages/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2b3793b2b06719a024f8",
+    "url": "/chronos-pages/static/css/main.d09b5046.chunk.css"
+  },
+  {
+    "revision": "4ecbeea32e2dafb6149fc30b3750d2ec",
+    "url": "/chronos-pages/index.html"
+  }
+];
